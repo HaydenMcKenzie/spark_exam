43fc7797f42cf57770b032a1408d324ebd02d16f
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2023-12-31 16:02:56 +1100

    testing my knowledge

--- src/main/scala/com/sundogsoftware/spark/FriendsByAgeTest.scala
+++ src/main/scala/com/sundogsoftware/spark/FriendsByAgeTest.scala
@@ -1,12 +1,13 @@
 package com.sundogsoftware.spark
 
-import org.apache.spark.sql._
 import org.apache.log4j._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object FriendsByAgeTest extends App {
 
   // Schema
-  case class Person(id: Int, name: String, age: Int, friends: Int)
+  case class Person(id: Int, name: String, age: Int, friends: Long)
 
   // Main program
 
@@ -14,7 +15,7 @@ object FriendsByAgeTest extends App {
 
   val spark = SparkSession
     .builder
-    .appName("SparkSQL")
+    .appName("FriendsByAge")
     .master("local[*]")
     .getOrCreate()
 
@@ -28,8 +29,20 @@ object FriendsByAgeTest extends App {
   println("Here is our inferred schema:")
   people.printSchema()
 
-
+  println("My Answer")
   people.groupBy("age").avg("friends").show()
+
+  // Need to add import functions and sparksession
+  val friendsByAge = people.select("age", "friends")
+
+  println("Sort By Age")
+  friendsByAge.groupBy("age").avg("friends").sort("age").show()
+
+  friendsByAge.groupBy("age").agg(round(avg("friends"), 2))
+    .sort("age").show()
+
+  friendsByAge.groupBy("age").agg(round(avg("friends"), 2)
+    .alias("friends_avg")).sort("age").show()
 
   spark.stop()
 }
