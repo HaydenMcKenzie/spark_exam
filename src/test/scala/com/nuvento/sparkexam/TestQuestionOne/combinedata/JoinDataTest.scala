3c6941bb732e0094fe68e8aff4c12e01bc0578bd
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2024-03-01 08:54:43 +1100

    JoinDataTest.scala: I added another test where I used schemas in Schemas.scala to build testing data without making a Seq. I also edited a test to make a fixed lines of data ReadDataTest.scala: I created expected lines of Data and Schema tests on all csv files SchemasTest.scala: I created schema tests for all csv files SparkSetupTest.scala: I created a test that makes sure that my SparkSession does start

--- src/test/scala/com/nuvento/sparkexam/TestQuestionOne/combinedata/JoinDataTest.scala
+++ src/test/scala/com/nuvento/sparkexam/TestQuestionOne/combinedata/JoinDataTest.scala
@@ -1,18 +1,36 @@
 import com.nuvento.sparkexam.combinedata.JoinData
 import com.nuvento.sparkexam.handlefiles.ReadData.readFileData
 import com.nuvento.sparkexam.handlefiles.Schemas
+import com.nuvento.sparkexam.handlefiles.Schemas.{accountSchema, customerSchema, addressSchema}
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.BeforeAndAfter
 import com.nuvento.sparkexam.utils._
-import org.apache.spark.sql.types.{StringType, StructField, StructType, IntegerType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 class JoinDataTest extends AnyFunSuite with BeforeAndAfter {
   SparkSetup.main(Array.empty[String])
   import SparkSetup.spark.implicits._
 
-
   test("Test joinData function on fixed data") {
+    // Sample input data
+    val firstData = customerSchema("1", "Alice", "Smith")
+    val firstTestingData: Dataset[customerSchema] = Seq(firstData).toDS()
+
+    val secondData = accountSchema("1", "ACC001", 100.0)
+    val secondTestingData: Dataset[accountSchema] = Seq(secondData).toDS()
+
+    // Call the function
+    val result = JoinData.joinData(firstTestingData, secondTestingData)
+
+    // Expected result
+    val expected = Seq(("1", "Alice", "Smith", "ACC001", 100.0)).toDF("customerId", "forename", "surname", "accountId", "balance")
+
+    // Compare the result with the expected output
+    assert(result.collect().sameElements(expected.collect()))
+  }
+
+  test("Test joinData function on multiple fixed data") {
     // Sample input data
     val firstData = Seq((1, "Alice"), (2, "Bob"), (3, "Charlie")).toDF("customerId", "name")
     val secondData = Seq((1, "Account1"), (2, "Account2"), (4, "Account4")).toDF("customerId", "accountName")
@@ -27,7 +45,7 @@ class JoinDataTest extends AnyFunSuite with BeforeAndAfter {
     assert(result.collect().sameElements(expected.collect()))
   }
 
-  test("Test joinData function Count more than 0") {
+  test("Test joinData function must be 545 lines of data") {
     // Input data
     val firstData = readFileData[Schemas.customerSchema]("customer_data")
     val secondData = readFileData[Schemas.accountSchema]("account_data")
@@ -35,8 +53,11 @@ class JoinDataTest extends AnyFunSuite with BeforeAndAfter {
     // Call the function
     val joinedData: Dataset[_] = JoinData.joinData(firstData, secondData)
 
+    // Expected value
+    val expected = 545
+
     // Test if it is has more than 0
-    assert(joinedData.count() > 0)
+    assert(joinedData.collect().length == expected)
   }
 
   test("Test joinData function Schema") {
--- src/test/scala/com/nuvento/sparkexam/TestQuestionOne/handlefiles/ReadDataTest.scala
+++ src/test/scala/com/nuvento/sparkexam/TestQuestionOne/handlefiles/ReadDataTest.scala
@@ -1,5 +1,109 @@
 package com.nuvento.sparkexam.TestQuestionOne.handlefiles
 
-object ReadDataTest {
+import com.nuvento.sparkexam.handlefiles.ReadData.readFileData
+import com.nuvento.sparkexam.handlefiles.Schemas
+import com.nuvento.sparkexam.utils.SparkSetup
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.scalatest.BeforeAndAfter
+import org.scalatest.funsuite.AnyFunSuite
 
+class JoinDataTest extends AnyFunSuite with BeforeAndAfter {
+  SparkSetup.main(Array.empty[String])
+  import SparkSetup.spark.implicits._
+
+  test("Test readFileData function on customer_data.csv must returns 500 lines of data") {
+    // Input Data
+    val testingData = readFileData[Schemas.customerSchema]("customer_data")
+
+    // Call the function
+    val result = testingData.collect().length
+
+    // Expected result
+    val expected = 500
+
+    // Compare the result with the expected output
+    assert(result == expected)
+  }
+  test("Test readFileData function customer_data.csv schema") {
+    // Input Data
+    val testingData = readFileData[Schemas.customerSchema]("customer_data")
+
+    // Call the function
+    val result = testingData.schema
+
+    // Expected result
+    val expected = StructType(Seq(
+      StructField("customerId", StringType, nullable = true),
+      StructField("forename", StringType, nullable = true),
+      StructField("surname", StringType, nullable = true)
+    ))
+
+    // Compare the result with the expected output
+    assert(result == expected)
+  }
+
+
+  test("Test readFileData function on account_data.csv must returns 600 lines of data") {
+    // Input Data
+    val testingData = readFileData[Schemas.accountSchema]("account_data")
+
+    // Call the function
+    val result = testingData.collect().length
+
+    // Expected result
+    val expected = 600
+
+    // Compare the result with the expected output
+    assert(result == expected)
+  }
+  test("Test readFileData function account_data.csv schema") {
+    // Input Data
+    val testingData = readFileData[Schemas.accountSchema]("account_data")
+
+    // Call the function
+    val result = testingData.schema
+
+    // Expected result
+    val expected = StructType(Seq(
+      StructField("customerId", StringType, nullable = true),
+      StructField("accountId", StringType, nullable = true),
+      StructField("balance", IntegerType, nullable = true)
+    ))
+
+    // Compare the result with the expected output
+    assert(result == expected)
+  }
+
+
+  test("Test readFileData function on address_data.csv must returns 500 lines of data") {
+    // Input Data
+    val testingData = readFileData[Schemas.addressSchema]("address_data")
+
+    // Call the function
+    val result = testingData.collect().length
+
+    // Expected result
+    val expected = 500
+
+    // Compare the result with the expected output
+    assert(result == expected)
+  }
+  test("Test readFileData function address_data.csv schema") {
+    // Input Data
+    val testingData = readFileData[Schemas.addressSchema]("address_data")
+
+    // Call the function
+    val result = testingData.schema
+
+    // Expected result
+    val expected = StructType(Seq(
+      StructField("addressId", StringType, nullable = true),
+      StructField("customerId", StringType, nullable = true),
+      StructField("address", StringType, nullable = true)
+    ))
+
+    // Compare the result with the expected output
+    assert(result == expected)
+  }
 }
+
