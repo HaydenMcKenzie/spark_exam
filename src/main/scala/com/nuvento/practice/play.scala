b109b09f66e2dbc4bb17487fdabe0326fd838c52
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2024-03-09 23:08:39 +1100

    AddColumn.scala: I added a new function where add all data from address_data.csv into a Seq[] QuestionTwo.scala: I made basic logic for QuestionTwo where it would work

--- src/main/scala/com/nuvento/practice/play.scala
+++ src/main/scala/com/nuvento/practice/play.scala
@@ -1,8 +1,10 @@
 package com.nuvento.practice
 
 import com.nuvento.sparkexam.SetUp
-import com.nuvento.sparkexam.SetUp.{accountData, customerData}
-import org.apache.spark.sql.Dataset
+import com.nuvento.sparkexam.SetUp.{accountData, addressData, customerData}
+import com.nuvento.sparkexam.comebinedata.TransformData.stringToSeq
+import com.nuvento.sparkexam.utils.SparkSetup
+import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{IntegerType, LongType}
 
@@ -29,4 +31,32 @@ object play extends App {
   println(test.count())
   filteredData.show()
 
+  val testString = stringToSeq(addressData, "address")
+  val grabColumn = testString.select("address").toDF()
+  grabColumn.collect()
+
+  val grabColumnTest = testString.withColumn("address", concat_ws(", ", col("addressId"), col("customerId"), col("address")))
+  println("AddressData")
+  val x = grabColumnTest.select("address").collect()
+  x.take(2).foreach(println)
+
+  """
+  import org.apache.spark.sql.functions.{col, lit}
+
+
+  val addressDF = grabColumn
+    .select(
+      col("address").getItem(0).cast("int").alias("number"),
+      col("address").getItem(1).alias("road"),
+      col("address").getItem(2).alias("city"),
+      col("address").getItem(3).alias("country")
+    )
+    .withColumn("number", when(col("number").isNull, lit(null)).otherwise(col("number")))
+    .withColumn("road", when(col("road").isNull, lit(null)).otherwise(col("road")))
+    .withColumn("city", when(col("city").isNull, lit(null)).otherwise(col("city")))
+    .withColumn("country", when(col("country").isNull, lit(null)).otherwise(col("country")))
+
+  println("New")
+  addressDF.show()
+  """
 }
--- src/main/scala/com/nuvento/sparkexam/QuestionTwo.scala
+++ src/main/scala/com/nuvento/sparkexam/QuestionTwo.scala
@@ -3,26 +3,31 @@ package com.nuvento.sparkexam
 import com.nuvento.sparkexam.comebinedata.TransformData.{removeColumns, stringToSeq}
 import com.nuvento.sparkexam.comebinedata.JoinData
 import com.nuvento.sparkexam.SetUp.{addressData, parquetFilePath}
+import com.nuvento.sparkexam.comebinedata.AddColumn.grabColumn
 import com.nuvento.sparkexam.handlefiles.ReadData.readParquetFile
 
 object QuestionTwo extends App {
-  """
   // Spark Setup
   SetUp.main(Array.empty[String])
 
   try {
     // Transforming the Data
-    val parquetFile = readParquetFile(parquetFilePath)
+    val parquetFile = readParquetFile("output")
 
     val removedColumnsForFirstData = removeColumns(parquetFile, "numberAccounts, totalBalance, averageBalance")
     val removedColumnsForSecondFile = removeColumns(addressData, "addressId")
+  removedColumnsForSecondFile.show()
 
-    val joinedColumnsThatHaventBeenRemoved = JoinData.joinData(removedColumnsForFirstData, removedColumnsForSecondFile, "customerId")
-    val processData = stringToSeq(joinedColumnsThatHaventBeenRemoved, "address")
+    val joinedColumnsThatHaventBeenRemoved = JoinData.joinData(removedColumnsForFirstData, removedColumnsForSecondFile, "customerId", "left")
+    val removedColumnsForThirdFile = removeColumns(joinedColumnsThatHaventBeenRemoved, "address")
+  removedColumnsForThirdFile.show()
 
+    val processData = grabColumn(addressData, "address").select( "customerId","address")
+  processData.show()
+    val test = JoinData.simpleJoinData(removedColumnsForThirdFile,processData,"customerId")
     // Printing
-    processData.show()
+    test.show()
   } catch {
     case e: Exception => println(s"File Does Not Exists.")
-  }"""
+  }
 }
