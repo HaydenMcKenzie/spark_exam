25c13453abceccd9fd107cf1dfbe1f607d459d9f
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2024-03-10 18:51:59 +1100

    Commiting To Keep As Storage

--- src/main/scala/com/nuvento/practice/play.scala
+++ src/main/scala/com/nuvento/practice/play.scala
@@ -2,14 +2,52 @@ package com.nuvento.practice
 
 import com.nuvento.sparkexam.SetUp
 import com.nuvento.sparkexam.SetUp.{accountData, addressData, customerData}
+import com.nuvento.sparkexam.comebinedata.AddColumn.testGrabColumn
 import com.nuvento.sparkexam.comebinedata.TransformData.stringToSeq
 import com.nuvento.sparkexam.utils.SparkSetup
-import org.apache.spark.sql.{DataFrame, Dataset}
+import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Encoders}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{IntegerType, LongType}
 
 object play extends App {
   SetUp.main(Array.empty[String])
+
+
+  testGrabColumn(addressData, "address").show()
+
+  // Step 1: Split address column
+  val addressSplit = addressData
+    .withColumn("addressComponents", split(col("address"), ","))
+    .select(
+      col("addressId").as("addressIdTwo"),
+      col("customerId").as("customerIdTwo"),
+      col("address").as("addressTwo"),
+      col("addressComponents").getItem(0).cast(IntegerType).as("number"),
+      col("addressComponents").getItem(1).as("road"),
+      col("addressComponents").getItem(2).as("city"),
+      col("addressComponents").getItem(3).as("country")
+    )
+
+  // Step 2: Merge with existing DataFrame
+  val mergedData = addressData.join(addressSplit, addressData("customerId") === addressSplit("customerIdTwo"), "left")
+
+  // Step 3: Define case class
+  case class AddressData2(
+                           addressIdTwo: String,
+                          customerIdTwo: String,
+                          addressTwo: String,
+                          number: Option[Int],
+                          road: Option[String],
+                          city: Option[String],
+                          country: Option[String]
+                        )
+  implicit val AddressData2Encoder: Encoder[AddressData2] = Encoders.product[AddressData2]
+  // Step 4: Create new DataFrame with updated schema
+  val processDataa = mergedData
+    .select("addressIdTwo", "customerIdTwo", "addressTwo", "number", "road", "city", "country")
+    .as[AddressData2]
+
+  processDataa.show()
 
   def aggregatedDataSet(data: Dataset[_]): Dataset[_] = {
     data.groupBy("customerId", "forename", "surname")
